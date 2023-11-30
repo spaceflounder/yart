c07157c569110b128595bab59635340b044fd3f8
spaceflounder <spaceflounder@hotmail.com>
2023-11-29 17:43:49 -0800

    Fixed achievements bug

---
+++ achievements.js
@@ -0,0 +1,35 @@
+
+function initializeAchievements() {
+
+    let totalScore = 0;
+    if (!story.achievements) {
+        story.achievements = {};
+    }
+    for (const achievement in story.achievements) {
+        const a = {...story.achievements[achievement]} ?? {};
+        const score = a?.score ?? 0;
+        //const content = a?.content ?? achievement;
+        totalScore += score;
+        story.achievements[achievement]['awarded'] = false;
+    }
+    story.achievements.total = totalScore;
+    story.achievements.score = 0;
+
+}
+
+
+function handleAward(award) {
+
+    if (!story['achievements'][award]['awarded']) {
+        story['achievements'][award]['awarded'] = true;
+        const score = story['achievements'][award]?.score ?? 0;
+        const content = story['achievements'][award]?.content ?? award;
+        story['achievements'].score += score;
+        bufferText += `Achievement: ${content}
+        
+        [Your score has just gone up ${score} points.]`;
+        console.log(bufferText);            
+    }    
+
+}
+
